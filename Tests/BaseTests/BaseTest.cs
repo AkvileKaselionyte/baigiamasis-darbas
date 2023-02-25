bd09e9a5ff416675e64f468b6763ea372b776ea5
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-25 17:04:34 +0200

    Add base test

---
+++ Tests/BaseTests/BaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Framework;
+using NUnit.Framework;
+
+namespace Tests.BaseTests
+{
+    internal class BaseTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.SetupDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.CloseDriver();
+        }
+    }
+}
