90196547b11c6320d73ab1472b3a26148e29ffa9
AkvileKaselionyte <akvile960608@gmail.com>
2023-02-25 16:56:17 +0200

    Add driver class

--- Framework/Driver.cs
+++ Framework/Driver.cs
@@ -1,6 +1,30 @@
-﻿namespace Framework
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace Framework
 {
     internal class Driver
     {
+        private static IWebDriver driver;
+
+        public static void SetupDriver()
+        {
+            driver = new ChromeDriver();
+        }
+
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        public static void OpenUrl(string url)
+        {
+            driver.Url = url;
+        }
+
+        public static void CloseDriver()
+        {
+            driver.Quit();
+        }
     }
 }
